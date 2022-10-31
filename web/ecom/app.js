f0dd10a3e5ba15f7d132e3a2f15f5da756dde34e
Anjana <66191342+anj-ana@users.noreply.github.com>
2022-10-31 09:42:54 -0700

    Productbutton

--- web/ecom/app.js
+++ web/ecom/app.js
@@ -97,4 +97,39 @@ menuItem.forEach((item,index)=>{
         currentProductImg.src = choosenProduct.colors[0].img;
         //assign new color
         currentProductColors.forEach((color,index) =>{
-            
+         color.style.backgroundColor = choosenProduct.colors[index].code;
+        });
+    });
+    
+});
+
+currentProductColors.forEach((color,index)=>{
+    color.addEventListener("click", () =>{
+        currentProductImg.src = choosenProduct.colors[index].img
+    });
+}
+);
+
+currentProductSizes.forEach((size,index) =>{
+    size.addEventListener("click",()=>{
+        currentProductSizes.forEach(size=>{
+            size.style.backgroundColor="white";
+            size.style.color="black";
+
+        })
+        size.style.backgroundColor="black";
+        size.style.color="white";
+    });
+}
+);
+const productButton = document.querySelector(".productButton");
+const payment = document.querySelector(".payment");
+const close = document.querySelector(".close");
+
+productButton.addEventListener("click",()=>{
+    payment.style.display = "flex"
+})
+
+close.addEventListener("click",()=>{
+    payment.style.display = "none"
+})   
